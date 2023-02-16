20b30f8e834823cb6f95c1a1e37c409f67ca3daf
Andrey <vbg_gtr@mail.ru>
2023-02-16 17:53:23 +0300

    task004

---
+++ Task003/Task004/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+int count = 1;
+int n = 3;
+
+
+    
+    int N = т
+    int index = 0;
+    int znach = 0;
+    int countChet = (N / 2);
+
+    while (index < countChet)
+    {
+        znach = znach + 2;
+        Console.Write(znach);
+        Console.Write(", ");
+        index = index + 1;
+    }
+    if (index == 0) Console.Write("Четных чисел нет ");
+    count = count + 1;
+    Console.WriteLine();
+
+		
+	
